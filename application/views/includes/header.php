457cc3f7380401b8f8761a6a07e7f88a26286724
Haq Nawaz <haqnawazwgbm@gmail.com>
2017-11-17 16:01:08 +0500

    Changed title
    Changed blog to CRUD title

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -3,7 +3,7 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Blog</title>
+    <title>CRUD</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
     <script
 	  src="https://code.jquery.com/jquery-3.2.1.min.js"
